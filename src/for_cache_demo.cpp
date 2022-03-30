4bf6b5e8622750d390fbe6c97c926634a87464dc
Rafael Rey <reyarcenegui@gmail.com>
2022-03-30 20:21:40 +0200

    Added cache effects simple demo script

--- src/for_cache_demo.cpp
+++ src/for_cache_demo.cpp
@@ -2,39 +2,62 @@
 #include <cstdlib>
 #include <chrono>
 #include <vector>
+#include <string>
+#include <cmath>
 
-#define SIZE 64 * 1024 * 1024
-#define K 16
 
-int main() {
+//Comment this to use C array instead of std vector
+//#define USE_C_ARRAY 
 
-	std::vector<int> arr(SIZE);
+int test(int array_size, int step, int tries) {
 
-	std::cout << "Step:           " << K << " Elements \n";
-	std::cout << "Array Size:     " << SIZE/(1024*1024) << " MBs \n";
-	std::cout << "Array Elements: " << arr.size() << "\n";
+	int total_time = 0;
+	for (int tr = 0; tr < tries; tr++) {
+
+#if defined(USE_C_ARRAY)
+		int* arr = new int[array_size];
+#else 
+		std::vector<int> arr(array_size);
+#endif
+
+		auto start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < array_size; i += step) arr[i]++;
+
+		total_time += std::chrono::duration_cast<std::chrono::milliseconds>((std::chrono::high_resolution_clock::now() - start)).count();
+
+#if defined(USE_C_ARRAY)
+		delete[] arr;
+#endif
+	}
+	return total_time/tries;
+}
+int main(int argc, char** argv) {
+
+	if (argc < 4)
+		return EXIT_FAILURE;
+	//First arg Size in MB
+	//Second arg Step 
+	int size, k, tries;
+	try
+	{
+		size  = std::stoi(std::string(argv[1])) * 1024 * 1024;
+		k     = std::stoi(std::string(argv[2]));
+		tries = std::stoi(std::string(argv[3]));
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Bad arguments. Usage ./program SIZE STEP NUM_TRIES arguments should be integers" << std::endl;
+		return EXIT_FAILURE;
+	}
+	/*
+	
+	std::cout << "Step:           " << k << " Elements \n";
+	std::cout << "Array Elements: " << size << "\n";
+	std::cout << "Array Size:     " << size/(1024*1024) << " MBs \n";
 	std::cout << "Int Size:       " << sizeof(int) << "\n";
-
-	auto show_duration = [&](auto title, auto duration){ 
-		std::cout << "[" << title << "] Duration: " << 
-			std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() <<
-			" (milisecs) \n";
-	};
-
-	auto start = std::chrono::steady_clock::now();
-	// Loop 1
-	for (int i = 0; i < SIZE; i++) arr[i]++;
-	show_duration("FullArray", 
-				(std::chrono::steady_clock::now() - start));
-
-
-	start = std::chrono::steady_clock::now();
-	// Loop 1
-	for (int i = 0; i < SIZE; i += K) arr[i]++;
-
-	show_duration(std::string("Array Step ") + std::to_string(K), 
-				(std::chrono::steady_clock::now() - start));
-
-
+	std::cout << "[" << k <<" Step]: " << test(size, k) << " microsecs\n" << std::endl;
+	*/
+	std::cout << "Testing with size " << size << " and step " << k << " for " << tries << " tries: " << test(size, k, tries) << " ms" << "\n";
+	std::cout << "Accesing same number of elements (contiguous): " << test(std::floor(size/k), 1, tries) << " ms" << std::endl;
 	return EXIT_SUCCESS;
 }
